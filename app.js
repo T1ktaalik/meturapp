7360aec39a992167a15fdf37adadbd2ac6c3cf34
Sergey <88627980+T1ktaalik@users.noreply.github.com>
2022-03-09 01:21:10 +0300

    09032022v1

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-//CORS module 
-const cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
